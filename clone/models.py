299168e9137f5ff1a8637098a2680f5ec9429b54
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-06-04 17:36:09 +0300

    feature:added add image field

--- clone/models.py
+++ clone/models.py
@@ -1,3 +1,4 @@
+from importlib.resources import contents
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -5,12 +6,24 @@ from django.contrib.auth.models import User
 class Image(models.Model):
     img_name=models.CharField(max_length=100)
     image=models.ImageField(upload_to='image/',null=True)
+    description=models.TextField(null=True)
     user=models.ForeignKey(User,on_delete=models.CASCADE)
     image_caption=models.CharField(max_length=200)
+    size=models.CharField(max_length=50, null=True)
     profile=models.ForeignKey('Profile', on_delete = models.CASCADE)
-    likes=models.CharField(max_length=100)
+    likes=models.IntegerField()
     comments=models.CharField(max_length=300)
 
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.save_image
+        self.delete()
+
+    def update_caption(self,id,image_caption):
+        updated_caption=Image.objects.filter(id=id).update(image_caption)
+        return updated_caption
 
     def __str__(self):
         return self.img_name
@@ -20,8 +33,28 @@ class Profile(models.Model):
     profile_img=models.ImageField(upload_to='image/',null=True)
     bio=models.TextField()
 
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.save_profile
+        self.delete()
+
+    def update_profile(self,id,profile):
+        updated_profile=Profile.objects.filter(id=id).update(profile)
+        return updated_profile
     def __str__(self):
         return self.name
 
+class Comment(models.Model):
+    title=models.CharField(max_length=100)
+    comments=models.TextField()
+    
+    def __str__(self):
+        return self.title
 
+class Likes(models.Model):
+    likes=models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.likes
--- clone/views.py
+++ clone/views.py
@@ -1,6 +1,26 @@
+
 from django.shortcuts import render
+from PIL import Image as PILimage
+from .models import *
+
 
 # Create your views here.
 
 def index(request):
     return render(request,'pages/index.html')
+
+def addimage(request):
+    
+    if request.method=='POST':
+        image=request.FILES.get('photo')
+        image_=PILimage.open(image)
+        name=request.POST.get('name')
+        size=f'{image_.width} x {image_.height}'
+        description=request.POST.get('description')
+        comments=request.POST.get('comment')
+        comment_=Comment.objects.get(id=comments)
+        likes=request.POST.get('location')
+        likes_=Likes.objects.get(id=likes)
+        img=Image(name=name,image=image,description=description,size=size,likes=likes_,comments=comment_)
+        img.save_image()
+    return render(request,'pages/addImage.html')
