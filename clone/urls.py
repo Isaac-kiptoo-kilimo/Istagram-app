9b6ca3f5bd12c8f11318202dd1d9fd00bef157eb
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-06-05 11:41:31 +0300

    style:addedstyling to the login

--- clone/urls.py
+++ clone/urls.py
@@ -8,4 +8,5 @@ urlpatterns=[
     path('profile/',views.profile,name='profile'),
     path('register/',views.register,name='register'),
     path('login/',views.loginPage,name='login'),
+    path('login/',views.logoutUser,name='logout'),
 ]
