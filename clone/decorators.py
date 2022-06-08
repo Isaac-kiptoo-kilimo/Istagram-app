268bec6831dd24250a598282bab4e2dd9dadd99e
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-06-08 18:03:27 +0300

    deployment to heroku

--- clone/decorators.py
+++ clone/decorators.py
@@ -14,11 +14,11 @@ def unauthenticated_user(view_func):
 
 
 def admin_only(view_func):
-    def wrapper_func(request, *args, **kwargs):
+    def wrapper_func(request,  **kwargs):
         if not request.user.is_superuser:
             return redirect('index')
         else:
-            return view_func(request, *args, **kwargs)
+            return view_func(request, **kwargs)
 
     return wrapper_func
 
